d2a1d9316da5ca5388314dc3cd91a330a4d7ee93
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-21 13:53:05 +0200

    ADD exx 05 - bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -11,8 +11,28 @@ const name = 'Mario';
 // Dichiara la funzione qui.
 
 
+//standard function
+function greetTime(name/*, time*/){
+    const date = new Date
+    const time = date.getHours()
+    let greet = ""
+    const separator = " "
+    //console.log(time)
+    if(time<=13){
+        greet="Buongiorno"
+    }else if(time<=17){
+        greet="Buon pomeriggio"
+    }else{
+        greet="Buonasera"
+    }
+    const greetName=greet+separator+name
+
+    return(greetName)
+}
+
 // Invoca la funzione qui e stampa il risultato in console
-
-
+//const time=parseInt(prompt("Indica che ore sono: "))
+const greetName=greetTime(name/*, time*/)
+console.log(greetName)
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
