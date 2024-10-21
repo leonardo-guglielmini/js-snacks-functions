1f2082de1696f612babff294fd3f03fa33c6e0e5
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-21 13:23:56 +0200

    ADD ex 01 - Iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,33 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+/*
+//standard function
+function takeFirstLetter(array){
+    let firstLetters=[]
+    let name
+    for(let i=0; i<array.length; i++){
+        name=array[i]
+        firstLetters.push(name[0])
+    }
+    return firstLetters
+}
+*/
+
+//arrow function
+const takeFirstLetter = (array) =>{
+    let firstLetters=[]
+    let name
+    for(let i=0; i<array.length; i++){
+        name=array[i]
+        firstLetters.push(name[0])
+    }
+    return firstLetters
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const firstLetters=takeFirstLetter(names)
+console.log(firstLetters)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
