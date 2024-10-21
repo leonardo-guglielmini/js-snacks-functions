84a46beaf03b6a1f44c4332a3b85df58384945ce
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-21 13:34:55 +0200

    ADD ex 03 - Conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,8 +5,39 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+/*
+//standard function
+function findVowels(word){
+    const vowels=["a","e","i","o","u"]
+    let totalVowels = 0
+    for(let i=0; i< word.length;i++){
+        for(let j=0; j<vowels.length;j++){
+            if(vowels[j]===word[i]){
+                totalVowels++
+            }
+        }
+    }
+    return totalVowels
+}
+*/
+
+//arrow function
+const findVowels = (word) =>{
+    const vowels=["a","e","i","o","u"]
+    let totalVowels = 0
+    for(let i=0; i< word.length;i++){
+        for(let j=0; j<vowels.length;j++){
+            if(vowels[j]===word[i]){
+                totalVowels++
+            }
+        }
+    }
+    return totalVowels
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const totalVowels = findVowels(word)
+console.log(`Nella parola ${word} si trovano ${totalVowels} vocali`)
 
 
 
